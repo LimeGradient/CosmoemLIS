9a197d528218d569b7a8d1c36e53b6d845886c35
LimeGradient <fnafgamer1yt@gmail.com>
2026-04-05 16:55:02 -0400

    fix injector + launcher

--- server/injector/src/main.cpp
+++ server/injector/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <iostream>
+#include <thread>
 
 DWORD getPID(const wchar_t* name) {
     HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -11,29 +12,65 @@ DWORD getPID(const wchar_t* name) {
     return 0;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc <= 2) {
+        std::cout << "No CMD Args given. Launch with EXE path and DLL path." << std::endl;
+        return 0;
+    }
+
     const wchar_t* TARGET   = L"Life is Strange - Before the Storm.exe";
-    const char*    DLL_PATH = "C:\\Users\\limeg\\LIS_Multiplayer\\bin\\LISMultiplayerHost.dll";
+    const char* EXE_PATH = argv[1];
+    const char* DLL_PATH = argv[2];
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    CreateProcess(
+        EXE_PATH,
+        0,
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi
+    );
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 
     DWORD pid = getPID(TARGET);
-    if (!pid) { std::cout << "Process not found\n"; return 1; }
+    if (!pid) { 
+        std::cout << "Process not found\n"; return 1; 
+    }
 
     HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
 
-    // Write DLL path into target process memory
-    void* remoteMem = VirtualAllocEx(hProc, nullptr, strlen(DLL_PATH) + 1,
-                                      MEM_COMMIT, PAGE_READWRITE);
+    void* remoteMem = VirtualAllocEx(
+        hProc, 
+        nullptr, 
+        strlen(DLL_PATH) + 1,
+        MEM_COMMIT, PAGE_READWRITE
+    );
     WriteProcessMemory(hProc, remoteMem, DLL_PATH, strlen(DLL_PATH) + 1, nullptr);
 
-    // Spawn a remote thread that calls LoadLibraryA with our DLL path
-    HANDLE hThread = CreateRemoteThread(hProc, nullptr, 0,
+    HANDLE hThread = CreateRemoteThread(
+        hProc, 
+        nullptr, 
+        0,
         (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA"),
-        remoteMem, 0, nullptr);
+        remoteMem, 
+        0,
+         nullptr
+    );
 
     WaitForSingleObject(hThread, 1000);
     VirtualFreeEx(hProc, remoteMem, 0, MEM_RELEASE);
     CloseHandle(hThread);
     CloseHandle(hProc);
 
-    std::cout << "Injected!\n";
+    std::cout << "Injected!" << std::endl;
 }
